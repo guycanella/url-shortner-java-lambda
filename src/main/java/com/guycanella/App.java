fa4bf123612a1a10e5cb728b2724f7f1d716ccf5
Guilherme Canella <guycanella@gmail.com>
2025-09-03 18:42:48 -0300

    initial commit

---
+++ src/main/java/com/guycanella/App.java
@@ -0,0 +1,13 @@
+package com.guycanella;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
