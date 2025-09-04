f455e43d6bde753abb322e3f59eb648ccbb1ce72
Guilherme Canella <guycanella@gmail.com>
2025-09-04 16:39:26 -0300

    update handleRequest to accept ttl optional paramenter

--- src/main/java/com/guycanella/UrlShortenerHandler.java
+++ src/main/java/com/guycanella/UrlShortenerHandler.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.security.SecureRandom;
 import java.time.Instant;
@@ -26,10 +27,10 @@ public class UrlShortenerHandler implements
         String method = input.getHttpMethod();
 
         if (method.equalsIgnoreCase("POST")) {
-            return shortenUrl(input.getBody());
+            return shortenUrl(input.getBody(), context);
         } else if (method.equalsIgnoreCase("GET")) {
             String shortCode = path.substring(path.lastIndexOf('/') + 1);
-            return redirectUrl(shortCode);
+            return redirectUrl(shortCode, context);
         }
 
         return new APIGatewayProxyResponseEvent()
@@ -37,13 +38,49 @@ public class UrlShortenerHandler implements
                 .withBody("Unsupported operation.");
     }
 
-    private APIGatewayProxyResponseEvent shortenUrl(String originalUrl) {
-        String code = CodeGenerator.generateCode();
+    private APIGatewayProxyResponseEvent shortenUrl(String body, Context context) {
+        String originalUrl;
+        int ttlMinutes = 1;
 
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String,Object> map = mapper.readValue(body, Map.class);
+            originalUrl = (String) map.get("url");
+
+            if (map.containsKey("ttl")) {
+                try {
+                    Object ttlObject = map.get("ttl");
+
+                    if (ttlObject != null) {
+                        ttlMinutes = Integer.parseInt(ttlObject.toString());
+
+                        if (ttlMinutes < 1) ttlMinutes = 1;
+                        if (ttlMinutes > 525600) ttlMinutes = 525600;
+                    }
+                } catch(Exception ex) {
+                    context.getLogger().log("Invalid TTL, using default 1 minute." + ex.getMessage());
+                }
+            }
+        } catch (Exception exception) {
+            originalUrl = body;
+            if (originalUrl != null && originalUrl.startsWith("\"") && originalUrl.endsWith("\"")) {
+                originalUrl = originalUrl.substring(1, originalUrl.length() - 1);
+            }
+        }
+
+        if (originalUrl == null || originalUrl.trim().isEmpty()) {
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(400)
+                    .withBody("{ \"error\": \"URL is required.\" }");
+        }
+
+        if (!originalUrl.startsWith("https://") && !originalUrl.startsWith("http://")) {
+            originalUrl = "https://" + originalUrl;
+        }
+
+        String code = CodeGenerator.generateCode();
         long now = Instant.now().getEpochSecond();
-        int expires_1min = 60;
-//        int expires_24hour = (60 * 60 * 24);
-        long ttl = now + expires_1min;
+        long ttl = now + (ttlMinutes * 60L);
 
         Map<String, AttributeValue> item = new HashMap<>();
         item.put("shortCode", AttributeValue.builder().s(code).build());
@@ -52,52 +89,75 @@ public class UrlShortenerHandler implements
         item.put("expiresAt", AttributeValue.builder().n(String.valueOf(ttl)).build());
         item.put("clickCount", AttributeValue.builder().n("0").build());
 
-        dynamoDb.putItem(PutItemRequest.builder().tableName(TABLE_NAME).item(item).build());
+        try {
+            dynamoDb.putItem(PutItemRequest.builder().tableName(TABLE_NAME).item(item).build());
+        } catch (Exception exception) {
+            context.getLogger().log("Error saving at DynamoDB: " + exception.getMessage());
+
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(500)
+                    .withBody("{ \"error\": Failed to create short URL.\" }");
+        }
 
         String shortUrl = CUSTOM_DOMAIN + "/" + code;
 
         return new APIGatewayProxyResponseEvent()
                 .withStatusCode(200)
-                .withBody("{ url: "+ shortUrl + " }");
+                .withBody("{ \"url\": "+ shortUrl + " }");
     }
 
-    private APIGatewayProxyResponseEvent redirectUrl(String shortCode) {
+    private APIGatewayProxyResponseEvent redirectUrl(String shortCode, Context context) {
         Map<String,AttributeValue> key = new HashMap<>();
         key.put("shortCode", AttributeValue.builder().s(shortCode).build());
 
-        GetItemResponse response = dynamoDb.getItem(
-                GetItemRequest.builder().tableName(TABLE_NAME).key(key).build()
-        );
+        try {
+            GetItemResponse response = dynamoDb.getItem(
+                    GetItemRequest.builder().tableName(TABLE_NAME).key(key).build()
+            );
 
-        if (!response.hasItem()) {
+            if (!response.hasItem() || response.item().isEmpty()) {
+                return new APIGatewayProxyResponseEvent()
+                        .withStatusCode(404)
+                        .withBody("URL Not Found.");
+            }
+
+            Map<String,AttributeValue> item = response.item();
+            AttributeValue expiresAtAttr = item.get("expiresAt");
+
+            if (expiresAtAttr != null && expiresAtAttr.n() != null) {
+                long expiresAt = Long.parseLong(expiresAtAttr.n());
+
+                if (Instant.now().getEpochSecond() > expiresAt) {
+                    return new APIGatewayProxyResponseEvent()
+                            .withStatusCode(410)
+                            .withBody("URL expired");
+                }
+            }
+
+            String originalUrl = item.get("originalUrl").s();
+
+            try {
+                dynamoDb.updateItem(UpdateItemRequest.builder()
+                        .tableName(TABLE_NAME)
+                        .key(key)
+                        .updateExpression("SET clickCount = clickCount + :inc")
+                        .expressionAttributeValues(Map.of(":inc",
+                                AttributeValue.builder().n("1").build()))
+                        .build());
+            } catch (Exception exception) {
+                context.getLogger().log("Fail to update clickCount: " + exception.getMessage());
+            }
+
             return new APIGatewayProxyResponseEvent()
-                    .withStatusCode(404)
-                    .withBody("URL Not Found.");
-        }
-
-        Map<String,AttributeValue> item = response.item();
-        long expiresAt = Long.parseLong(item.get("expiresAt").n());
-
-        if (Instant.now().getEpochSecond() > expiresAt) {
+                    .withStatusCode(302)
+                    .withHeaders(Map.of("Location", originalUrl))
+                    .withBody("Redirecting to " + originalUrl);
+        } catch (Exception exception) {
+            context.getLogger().log("Error searching the URL: " + exception.getMessage());
             return new APIGatewayProxyResponseEvent()
-                    .withStatusCode(410)
-                    .withBody("URL expired");
+                    .withStatusCode(500)
+                    .withBody("Internal server error");
         }
-
-        String originalUrl = item.get("originalUrl").s();
-
-        dynamoDb.updateItem(UpdateItemRequest.builder()
-                .tableName(TABLE_NAME)
-                .key(key)
-                .updateExpression("SET clickCount = clickCount + :inc")
-                .expressionAttributeValues(Map.of(":inc",
-                        AttributeValue.builder().n("1").build()))
-                .build());
-
-        return new APIGatewayProxyResponseEvent()
-                .withStatusCode(302)
-                .withHeaders(Map.of("Location", originalUrl))
-                .withBody("Redirecting to " + originalUrl);
     }
 
     public static class CodeGenerator {
